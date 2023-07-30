7e48777c0e58dacf01c7bc70e402ae82dd111463
m-sabid <sabidofficial@gmail.com>
2023-07-30 16:53:26 +0600

    dashboard added side nav inside the layout file

--- src/components/dashboard/SidebarNav.jsx
+++ src/components/dashboard/SidebarNav.jsx
@@ -25,11 +25,28 @@ const menus = [
 const SidebarNav = () => {
   return (
     <div>
-      <ul>
-        {menus.map((menu, index) => (
-          <li key={index}>{menu.name}</li>
-        ))}
-      </ul>
+
+      <div className="drawer lg:drawer-open">
+        <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content flex flex-col items-center justify-center">
+          {/* Page content here */}
+          <label
+            htmlFor="my-drawer-2"
+            className="btn btn-primary drawer-button lg:hidden"
+          >
+            Open drawer
+          </label>
+        </div>
+        <div className="drawer-side">
+          <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
+          <ul className="menu p-4 w-80 h-full bg-base-200 text-base-content">
+            {/* Sidebar content here */}
+            {menus.map((menu, index) => (
+              <li key={index}>{menu.name}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
