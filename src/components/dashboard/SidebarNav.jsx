38041d65b3729e7dd4bfe75e6cdd2c7dff141aaa
m-sabid <sabidofficial@gmail.com>
2023-07-30 23:05:24 +0600

    dashboard added and pass children into sidenav

--- src/components/dashboard/SidebarNav.jsx
+++ src/components/dashboard/SidebarNav.jsx
@@ -22,14 +22,14 @@ const menus = [
   },
 ];
 
-const SidebarNav = () => {
+const SidebarNav = ({ children }) => {
   return (
-    <div>
-
+    <div className="absolute">
       <div className="drawer lg:drawer-open">
         <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
         <div className="drawer-content flex flex-col items-center justify-center">
           {/* Page content here */}
+          {children}
           <label
             htmlFor="my-drawer-2"
             className="btn btn-primary drawer-button lg:hidden"
