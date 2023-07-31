f62e3d82e8175ea125717d5be7cf252b2b5966ef
m-sabid <sabidofficial@gmail.com>
2023-07-31 12:55:37 +0600

    dashboard added some update on nav

--- src/components/dashboard/SidebarNav.jsx
+++ src/components/dashboard/SidebarNav.jsx
@@ -2,26 +2,23 @@
 
 import Link from "next/link";
 import React from "react";
-import { FaBell, FaChartBar, FaHome } from "react-icons/fa";
+import { FaFilm, FaFolderPlus, FaHome } from "react-icons/fa";
 
 const menus = [
   {
-    name: "Dashboard",
+    name: "Home",
     path: "",
-    icon: <FaHome width={18} className="text-blue-500" />,
-    isActive: true,
+    icon: <FaHome width={18} className="text-white" />,
   },
   {
-    name: "Analytics",
-    path: "/home",
-    isActive: true,
-    icon: <FaChartBar width={18} className="text-gray-600" />,
+    name: "Add industry",
+    path: "/add_industry",
+    icon: <FaFilm width={18} className="text-white" />,
   },
   {
-    name: "Notification",
-    path: "home",
-    isActive: false,
-    icon: <FaBell width={18} className="text-gray-600" />,
+    name: "Add cinemas",
+    path: "add_cinemas",
+    icon: <FaFolderPlus width={18} className="text-white" />,
   },
 ];
 
@@ -74,10 +71,20 @@ const SidebarNav = ({ children }) => {
         <div className="drawer-side">
           <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
           <ul className="menu p-4 w-80 h-full bg-gray-600 text-white">
+            <Link
+              href={"/"}
+              className="text-2xl hidden lg:flex justify-center items-center"
+            >
+              J4B Movies
+            </Link>
+            <div className="divider hidden lg:flex"></div>
             {/* Sidebar content here */}
             {menus.map((menu, index) => (
               <li key={index}>
-                <Link href={menu.path}>{menu.name}</Link>
+                <Link href={menu.path}>
+                  {menu.icon}
+                  {menu.name}
+                </Link>
               </li>
             ))}
           </ul>
