8410d4c458ebdeec406fe0a34f26b3c113b7aafe
m-sabid <sabidofficial@gmail.com>
2023-07-31 10:45:49 +0600

    dashboard added top bar and handel sidebar

--- src/components/dashboard/SidebarNav.jsx
+++ src/components/dashboard/SidebarNav.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import Link from "next/link";
 import React from "react";
@@ -31,14 +31,46 @@ const SidebarNav = ({ children }) => {
       <div className="drawer lg:drawer-open ">
         <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
         <div className="drawer-content flex flex-col">
-          {/* Page content here */}
-          <div className="p-8">{children}</div>
-          <label
-            htmlFor="my-drawer-2"
-            className="btn btn-primary drawer-button lg:hidden"
-          >
-            Open drawer
-          </label>
+          {/*  */}
+          {/*  */}
+          <div className="lg:hidden">
+            <div className="navbar bg-gray-500 text-white">
+              <div className="flex-none">
+                <button className="btn  btn-square btn-ghost">
+                  <label
+                    htmlFor="my-drawer-2"
+                    className="btn bg-gray-500 text-white drawer-button lg:hidden"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      className="inline-block w-5 h-5 stroke-current"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M4 6h16M4 12h16M4 18h16"
+                      ></path>
+                    </svg>
+                  </label>
+                </button>
+              </div>
+              <div className="flex-1">
+                <Link href={"/"} className="btn btn-ghost normal-case text-xl">
+                  J4B Movies
+                </Link>
+              </div>
+              <div className="flex-none"></div>
+            </div>
+
+            {/*  */}
+            {/*  */}
+
+            {/* Page content here */}
+            <div className="p-8">{children}</div>
+          </div>
         </div>
         <div className="drawer-side">
           <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
