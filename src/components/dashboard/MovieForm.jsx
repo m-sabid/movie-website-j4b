f2a482ba0a6813243bf0fb24c45a257d5d33fbc5
m-sabid <sabidofficial@gmail.com>
2023-08-01 15:44:15 +0600

    added multylang

--- src/components/dashboard/MovieForm.jsx
+++ src/components/dashboard/MovieForm.jsx
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 
 const MovieForm = () => {
   const { register, handleSubmit, errors } = useForm();
+  const [selectedLanguages, setSelectedLanguages] = useState([]);
+
+  const handleLanguageSelect = (e) => {
+    const selectedLanguage = e.target.value;
+    if (!selectedLanguages.includes(selectedLanguage)) {
+      setSelectedLanguages([...selectedLanguages, selectedLanguage]);
+    }
+  };
+
+  const handleTagRemove = (language) => {
+    setSelectedLanguages(selectedLanguages.filter((lang) => lang !== language));
+  };
 
   const onSubmit = (data) => {
     // Handle form submission here
@@ -12,7 +24,7 @@ const MovieForm = () => {
   return (
     <form
       onSubmit={handleSubmit(onSubmit)}
-      className="bg-gray-800 rounded-md w-full p-4"
+      className="bg-gray-500 rounded-md w-full p-4"
     >
       <div className="grid grid-cols-2 gap-4">
         <div className="mb-4 col-span-2 md:col-span-1">
@@ -26,20 +38,6 @@ const MovieForm = () => {
             className="input input-bordered input-accent w-full"
           />
           {errors?.movieName && (
-            <span className="text-red-500">This field is required</span>
-          )}
-        </div>
-
-        <div className="mb-4 col-span-2 md:col-span-1">
-          <label htmlFor="plot" className="block text-white font-bold">
-            Plot
-          </label>
-          <textarea
-            {...register("plot", { required: true })}
-            placeholder="Plot"
-            className="textarea textarea-accent w-full"
-          />
-          {errors?.plot && (
             <span className="text-red-500">This field is required</span>
           )}
         </div>
@@ -79,16 +77,54 @@ const MovieForm = () => {
           </label>
           <select
             {...register("language", { required: true })}
-            defaultValue="english" // Use defaultValue instead of defaultChecked
-            className="input input-bordered input-accent w-full"
+            onChange={handleLanguageSelect}
+            className="input input-bordered w-full"
           >
+            <option value="">Select a language</option>
             <option value="english">English</option>
             <option value="spanish">Spanish</option>
             <option value="french">French</option>
             {/* Add more language options as needed */}
           </select>
           {errors?.language && (
-            <span className="text-red-500">This field is required</span>
+            <span className="text-red-600">This field is required</span>
+          )}
+
+          <div className="mt-2">
+            {selectedLanguages.map((language) => (
+              <div
+                key={language}
+                className="inline-flex items-center bg-blue-500 text-white px-3 py-1 rounded-full text-sm mr-2 mt-2"
+              >
+                {language}
+                <button
+                  type="button"
+                  className="ml-2"
+                  onClick={() => handleTagRemove(language)}
+                >
+                  &#10005;
+                </button>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <div className="mb-4 col-span-2 md:col-span-1">
+          <label htmlFor="industry" className="block text-white font-bold">
+            Industry
+          </label>
+          <select
+            {...register("industry", { required: true })}
+            className="input input-bordered w-full"
+          >
+            <option value="">Select an industry</option>
+            <option value="english">English</option>
+            <option value="spanish">Spanish</option>
+            <option value="french">French</option>
+            {/* Add more industry options as needed */}
+          </select>
+          {errors?.industry && (
+            <span className="text-red-600">This field is required</span>
           )}
         </div>
 
@@ -140,13 +176,12 @@ const MovieForm = () => {
 
         <div className="mb-4 col-span-2 md:col-span-1">
           <label htmlFor="poster" className="block text-white font-bold">
-            Poster URL
+            Poster
           </label>
           <input
-            type="text"
+            type="file"
             {...register("poster", { required: true })}
-            placeholder="Poster URL"
-            className="input input-bordered input-accent w-full"
+            className="file-input file-input-bordered file-input-accent w-full"
           />
           {errors?.poster && (
             <span className="text-red-500">This field is required</span>
@@ -170,18 +205,32 @@ const MovieForm = () => {
 
         <div className="mb-4 col-span-2 md:col-span-1">
           <label htmlFor="screenShort" className="block text-white font-bold">
-            Screen Short URL
+            Screen Short
           </label>
           <input
-            type="text"
+            type="file"
             {...register("screenShort", { required: true })}
-            placeholder="Screen Short URL"
-            className="input input-bordered input-accent w-full"
+            className="file-input file-input-bordered file-input-accent w-full"
           />
           {errors?.screenShort && (
             <span className="text-red-500">This field is required</span>
           )}
         </div>
+
+        <div className="mb-4 col-span-2 md:col-span-2">
+          <label htmlFor="plot" className="block text-white font-bold">
+            Plot
+          </label>
+          <textarea
+            {...register("plot", { required: true })}
+            placeholder="Plot"
+            className="textarea textarea-accent w-full h-32"
+          />
+          {errors?.plot && (
+            <span className="text-red-500">This field is required</span>
+          )}
+        </div>
+
       </div>
 
       <input type="submit" value="Submit" className="btn btn-primary w-full" />
