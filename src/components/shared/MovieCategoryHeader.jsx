0332c333ae26fc451c1899643a417e67d8079854
m-sabid <sabidofficial@gmail.com>
2024-11-27 16:52:15 +0600

    fix a bug

--- src/components/shared/MovieCategoryHeader.jsx
+++ src/components/shared/MovieCategoryHeader.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { FaFilter } from "react-icons/fa";
+import TypographyWrapper from "./TypographyWrapper";
 
 const MovieCategoryHeader = ({ title }) => {
   return (
